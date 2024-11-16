7e77c9a595f52fd75c8d64e835d02e303dd97846
Dhaval Savalia <hello@dhavalsavalia.com>
2024-11-16 18:37:52 -0500

    remove echo from sourcing zsh
    why am I an idiot?

--- .config/zsh/conf.d/30-tools/zsh-utils.zsh
+++ .config/zsh/conf.d/30-tools/zsh-utils.zsh
@@ -1,2 +1,2 @@
-echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
