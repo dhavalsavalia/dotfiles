3ca2fd47852be82ef0e3d2e090ada09302524ad4
Dhaval Savalia <hello@dhavalsavalia.com>
2024-11-16 14:51:07 -0500

    add zsh keybindings

---
+++ .config/zsh/conf.d/02-key-bindings.zsh
@@ -0,0 +1,3 @@
+bindkey '^[[A' history-substring-search-up  # Up for history
+bindkey '^[[B' history-substring-search-down  # Down for history
+bindkey '^[^?' backward-kill-word  # Alt + Backspace to delete word
