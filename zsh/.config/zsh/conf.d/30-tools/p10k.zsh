48fd19b9d0a7a565c01c4ec0944b38b1cedb8918
Dhaval Savalia <hello@dhavalsavalia.com>
2024-11-23 16:47:26 -0500

    try p10k

---
+++ zsh/.config/zsh/conf.d/30-tools/p10k.zsh
@@ -0,0 +1,6 @@
+if [ -f "$(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" ]; then
+    source "$(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme"
+fi
+
+
+# echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
